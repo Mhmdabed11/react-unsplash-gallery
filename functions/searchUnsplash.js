8cc0e5373c022105769b76523e65212a3be15a9e
Mohammad <mohd.abed@saugo360.com>
2020-05-08 16:56:53 +0300

    add unsplash url

--- functions/searchUnsplash.js
+++ functions/searchUnsplash.js
@@ -1,6 +1,14 @@
-exports.handler = function (event, context, callback) {
+const fetch = require("node-fetch");
+const UNSPLASH_URL = "https://api.unsplash.com/search/photos?query=office";
+exports.handler = async function (event, context, callback) {
+    console.log(process.env.ACCESS_TOKEN);
+    // const res = await fetch(UNSPLASH_URL, {
+    //     headers: { Authorization: `Client-ID C59rswL-bq7lJeQ23ZGEslVSW1bewnHiI9WNJ-FFEOk` },
+    // });
+    // const data = await res.json();
+    const data = { name: "hey" };
     callback(null, {
         statusCode: 200,
-        body: JSON.stringify({ name: "hey" }),
+        body: JSON.stringify(data),
     });
 };
